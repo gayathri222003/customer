95e3498ee819bb5d9bc805622422ba52f54c50d9
Gayathri-039 <01fe22bca039@kletech.ac.in>
2024-02-03 12:19:47 +0530

    updated the code for GoArea

---
+++ area/GoArea.java
@@ -0,0 +1,31 @@
+/**
+ * The GoArea class contains methods to calculate the area of a triangle and a rectangle.
+ */
+public class GoArea
+{
+   
+    double area_of_triangle(double operand_1, double operand_2)
+    {
+        return(0.5 * operand_1 * operand_2);
+    }
+    /**
+     * The function calculates the area of a rectangle given its length and width.
+     * 
+     * @param operand_1 The first operand of the rectangle, which represents the length of one side of
+     * the rectangle.
+     * @param operand_2 The second operand is the length of the rectangle.
+     * @return the area of a rectangle, which is the product of the two operands.
+     */
+    /**
+     * The function calculates the area of a rectangle given its length and width.
+     * 
+     * @param operand_1 The length of the rectangle.
+     * @param operand_2 The second operand is the length of the rectangle.
+     * @return the area of a rectangle, which is the product of the two operands.
+     */
+    double area_of_rectangle(double operand_1, double operand_2)
+    {
+        return(operand_1 * operand_2);
+    }
+    
+}
